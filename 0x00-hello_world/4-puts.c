0577595cab37e55a237be76b9ec39bf59545afc2
Lawrence-tech <lawrencesagini2@gmail.com>
2022-10-14 06:39:34 -0700

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main(void)
-/* Description: main -Prints out a string to stdout. */
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
 {
 puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
-/*return : 0 is a success*/
-
 }
