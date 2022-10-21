2a8ae87722ca535d0563ccfa9d1856a82c1349d1
Lawrence-tech <lawrencesagini2@gmail.com>
2022-10-20 18:55:46 -0700

    added another main file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - test that the function gives correct output for 0.
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
