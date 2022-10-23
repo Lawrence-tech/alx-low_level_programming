7aebedd485a1030ffdcc62197c9dd3482e52423f
Lawrence-tech <lawrencesagini2@gmail.com>
2022-10-23 03:34:06 -0700

    prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 through 9.
+ *Return: Always o.
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	if (c >= '48' && c <= '57')
+	{
+		_putchar('c');
+	}
+	else
+		return (0);
+	_putchar('\n):
+}
