95d10fb4e0bc8be8d0a983dd91437ad96b81ad1c
Lawrence-tech <lawrencesagini2@gmail.com>
2022-10-25 21:20:58 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,6 +26,13 @@ void print_times_table(int n)
 					}
 					_putchar(mult + '0');
 				}
+				else if (mult < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((mult / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
 				else
 				{
 					_putchar(' ');
