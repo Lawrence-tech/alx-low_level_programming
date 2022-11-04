d310f1f5b126bf85c082103544938086a01e5370
Lawrence-tech <lawrencesagini2@gmail.com>
2022-11-04 15:38:42 -0700

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  *is_prime_number - return 1for integer, 0 otherwise
+  *@n: input
+  *Return: int
+  */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (getPrimNum(n, 2));
+}
+
+/**
+  *getPrimNum - gets prim if num is 0 or 1
+  *@num: input
+  *@i: counter
+  *Return: int
+  */
+
+int getPrimNum(int num, int i)
+{
+	if (num == i)
+		return (1);
+	if (num % i == 0)
+		return (0);
+	return (getPrimNum(num, i + 1));
+}
