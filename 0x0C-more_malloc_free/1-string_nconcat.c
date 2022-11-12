7a8e52b3339c616681cc5edd18412de00ef5e58c
Lawrence-tech <lawrencesagini2@gmail.com>
2022-11-11 22:02:11 -0800

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,13 +30,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc(sizeof(*str) * n + (len1 + 1));
 	if (str == NULL)
 		return (NULL);
-	/*len1 = 0;*/
+	len1 = 0;
 	while (s1[len1] != '\0')
 	{
 		str[len1] = s1[len1];
 		len1 += 1;
 	}
-	/*len2 = 0;*/
+	len2 = 0;
 	while (s2[len2] != '\0' && len2 < n)
 	{
 		str[len1] = s2[len2];
