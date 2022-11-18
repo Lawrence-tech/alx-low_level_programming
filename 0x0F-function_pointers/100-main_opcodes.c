f3c90ad1b7af5c80ab309d427f48d4df3bee87de
Lawrence-tech <lawrencesagini2@gmail.com>
2022-11-17 19:00:21 -0800

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *main - prints opcodes
+  *@argc: count argument
+  *@argv: count vector
+  *Return: int
+  */
+
+int main (int argc, char **argv)
+{
+	int i, bytes;
+	char *addr;
+
+	if (argc !=2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n")
+			exit(2);
+	}
+	addr =(char *)main;
+	for (i = 0; i < bytes - 1; i++)
+		printf("%02hhx ", addr[i]);
+	return (0);
+}
