312ddac23c7a15b4d01cb4ec760ebe05aa02021c
Lawrence-tech <lawrencesagini2@gmail.com>
2022-11-20 07:53:23 -0800

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  *new_dog - creates a new dog.
+  *@name: dog name
+  *@age: dog age.
+  *@owner: dog owner
+  *Return: pointer to dog.
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
+
