65dad36266b9f205c8ec040884e6a9ce2f0288a5
Lawrence-tech <lawrencesagini2@gmail.com>
2022-12-11 02:22:21 -0800

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+  *create_file - Create a function that creates a file.
+  *@filename: is the name of the file to create
+  *@text_content: is a NULL terminated string to write to the file
+  *Return: 1 on success, -1 on failure
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int fd_read, inlen = 0;
+	char *buf;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (inlen = 0, buf = text_content; *buf; buf++)
+			inlen++;
+		fd_read = write(fd, text_content, inlen);
+	}
+
+	if (close(fd) == -1 || inlen != fd_read)
+		return (-1);
+	return (1);
+}
