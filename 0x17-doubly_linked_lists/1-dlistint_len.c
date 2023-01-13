c661b76b4d1c22d56458be4a3a2c61000c221829
Lawrence-tech <lawrencesagini2@gmail.com>
2023-01-12 18:58:52 -0800

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,7 +6,7 @@
 /**
   *dlistint_len - returns the number of elements in a linked list
   *@h: head of list to print
-  *Return: the number of nodes
+  *Return: length of list. 0 if does not exist
   */
 
 size_t dlistint_len(const dlistint_t *h)
