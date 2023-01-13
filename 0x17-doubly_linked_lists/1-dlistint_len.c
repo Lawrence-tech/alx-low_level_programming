e6cd6eba4c89fa43bb7875165a78416ee8e59500
Lawrence-tech <lawrencesagini2@gmail.com>
2023-01-13 01:29:33 -0800

    fix task 2 of  double linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,22 @@
 #include "lists.h"
-#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
   *dlistint_len - returns the number of elements in a linked list
-  *@h: head of list to print
+  *@h: head pointer. 
   *Return: length of list. 0 if does not exist
   */
 
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
+	const dlistint_t *p = h;
 
-	while (h != NULL)
+	while (p != NULL)
 	{
-		h = h->next;
+		p = p->next;
 		len++;
 	}
 	return (len);
