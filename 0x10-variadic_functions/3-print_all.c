1867003b630ba1195bd5c0237947f7b78670adc6
Lawrence-tech <lawrencesagini2@gmail.com>
2023-03-29 22:06:07 -0700

    fix print all requirement failure

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,85 +1,58 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 /**
-  *print_char - prints char
-  *@list: argument list
-  */
-
-void print_char(va_list list)
-{
-	printf("%c", (char) va_arg(list, int));
-}
-
-/**
-  *print_int - print int
-  *@list: argument list
-  */
-
-void print_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
-  *print_string - prints string
-  *@list: argument list
-  *Return: none
-  */
-
-void print_string(va_list list)
-{
-	char *str = va_arg(list, char *);
-
-	printf("%s", str ? str : "(nil)");
-}
-
-/**
-  *print_float - prints float
-  *@list: argument list
-  *Return: none
-  */
-
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-
-/**
-  *print_all - prints anything.
-  *@format: string containing type information for args.
-  *Return: arguments.
-  */
+ * print_all - Prints anything.
+ * @format: List of types of arguments.
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead)
+ * Any other char should be ignored
+ *
+ * Return: void.
+ */
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	const char *ptr;
-	int keyind = 0, notfirst = 0;
-	funck key[4] = {
-		{print_char, 'c'},
-		{print_int, 'i'},
-		{print_string, 's'},
-		{print_float, 'f'},
-	};
+	va_list ap;
+	const char *p = format;
+	char *s;
 
-	va_start(list, format);
-	ptr = format;
-	while (format != NULL && *ptr != '\0')
+	va_start(ap, format);
+
+	while (p && *p)
 	{
-		if (keyind > 3)
-			break;
-		if (key[keyind].spec == *ptr)
+		switch (*p++)
 		{
-			if (notfirst)
-				printf(", ");
-			notfirst = 1;
-			key[keyind].f(list);
-			keyind = -1;
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+
+			default:
+				continue;
 		}
-		ptr++;
-		keyind++;
+
+		if (*p)
+			printf(", ");
 	}
-	va_end(list);
+
 	printf("\n");
+
+	va_end(ap);
 }
